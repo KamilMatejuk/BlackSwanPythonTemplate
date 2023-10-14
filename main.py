bd2f65e5315548d419df4c11310fda75115076cb
Kamil Matejuk <kamilmatejuk@gmail.com>
2023-10-14 12:45:00 +0200

    add invalid request

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import connexion
+from flask import jsonify
 from decouple import Config, RepositoryEnv
 from flask_socketio import SocketIO
 
 
 def hello(name: str):
+    try:
+        assert name.lower() != "world", "Hello World is to basic"
+    except Exception as ex:
+        return jsonify({"error": "Invalid request schema", "details": str(ex)}), 401
+
     return f'Hello {name}', 200
 
 
